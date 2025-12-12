a5fb8c751727aba30375daa8bcf90f28fa2f811e
Traipop <traipopwichiansarn@gmail.com>
2025-12-12 16:32:34 +0700

    lab7_3 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    if((int(x)>=int('A'))&&(int(x)<=('Z'))){
+        if(int(x-1)<int('A')){
+            return 'Z';
+        } else {
+            return x-1;
+        }
+    } else {
+        return '0';
+    }
+
 }
 
 int main(){
