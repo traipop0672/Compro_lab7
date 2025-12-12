e5ad278820a013aad4e757eae4f5bded2ed48d8d
Traipop <traipopwichiansarn@gmail.com>
2025-12-12 16:19:26 +0700

    lab7_2 completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,18 +1,30 @@
-//Dialog of the first example is given below.
+#include<iostream>
+#include<string>
+using namespace std;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int main(){
+    string name,Id,movie,time,text;
+    int gear;
+    // cin >> Id;
+    gear = ((Id[0]-'0')*10+(Id[1]-'0'))-12;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! "<<name<<" is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID? \n";
+    cout << name<< ": ";
+    getline (cin,Id);
+    gear = ((Id[0]-'0')*10+(Id[1]-'0'))-12;
+    cout << "Fahsai: I think you may be GEAR "<< gear <<". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << name <<": ";
+    getline(cin,movie);
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name <<": ";
+    getline(cin,time);
+    cout << "Fahsai: "<<time<<"....that is OK!!! I'm looking forward to watching "<<movie<<" with you.\n";
+    cout << name <<": ";
+    getline(cin,text);
+    cout <<"Fahsai: 555+ see you "<<time<<". Bye Bye "<<"\\(^ ^)/";
+}
